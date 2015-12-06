c2aec36e79f9c9b02ddd75a7534ba5393a7bf71c
Edd <edoatley@gmail.com>
2015-12-06 14:25:31 +0000

    refined version of problem 7

--- project.euler/src/main/java/edo/project/euler/problem7/Problem7Refined.java
+++ project.euler/src/main/java/edo/project/euler/problem7/Problem7Refined.java
@@ -21,16 +21,27 @@ public class Problem7Refined {
 	 * No 2,3,5                     *             *       *               *       *               *                       *
 	 * Offset                       0             +4     +2              +4      +2              +4                      +6
 	 * 7 + 6n                       *                     *                       *                       *
+     * 6n and 6n+2            *     *             *       *               *       *               *       *               *    
+     * (starting 5)                   
+     * From website some useful facts
+     * 
+     * * 1 is not a prime.
+     * * All primes except 2 are odd.
+     * * All primes greater than 3 can be written in the form  6k+/-1.
+     * * Any number n can have only one primefactor greater than root n.
+     * * The consequence for primality testing of a number n is: 
+     *       if we cannot find a number f less than or equal root n 
+     *       that divides n then n is prime: the only primefactor of n is n itself
      * 
 	 * Can we do a simple function that gives numbers only if not divisible by 2,3,5 
 	 */
 	public long solve(int primeRequired) {
-		long candidate = 2;
-		long primesEncountered = 1;
+		long candidate = 1;
+		long primesEncountered = 0;
 		
 		while(primesEncountered < primeRequired) {
-			candidate++;
-			if((candidate % 2 == 1) && isPrime(candidate)) {
+			candidate += 2;
+			if(isPrime(candidate)) {
 				primesEncountered++;
 			}
 		}
@@ -38,22 +49,36 @@ public class Problem7Refined {
 		return candidate;
 	}
 	
-	private boolean isPrime(long testee) {
+	private boolean isPrime(long n) {
 		
-		if(testee == 2) {
+		if(n==1) {				// 1 is not prime
+			return false;
+		}
+		else if(n < 4) {		// 2 & 3 are prime
 			return true;
 		}
-		if(testee % 2 == 0) {
+		else if(n % 2 == 0) {	// even numbers except 2 are not prime
+			return false;
+		}
+		else if(n < 9) {		// 5 & 7 are prime
+			return true;
+		}
+		else if(n % 3 == 0) {	// numbers divisible by 3 except 3 are not prime
 			return false;
 		}
 		
-		long limit = testee / 2;
-		for (long i = 3; i < limit; i+=2) {
-			if(testee % i == 0) {
+		long limit = Double.valueOf(Math.sqrt(n)).longValue();
+		for (long f = 5; f <= limit; f+=6) {
+			if(n % f == 0) {
+				return false;
+			}
+			if((n % (f+2)) == 0) {
 				return false;
 			}
 		}
 		
 		return true;
 	}
+	
+	
 }
